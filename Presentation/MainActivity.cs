ccad836217b273cdaf7a97c6b4403c6f2719f15d
Antony Bailey <support@antonybailey.net>
2016-10-23 20:22:24 +0100

    Base working

---
+++ Presentation/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Views;
+using Android.Webkit;
+
+namespace Freshaus
+{
+    [Activity(Label = "Freshaus", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            RequestWindowFeature(WindowFeatures.NoTitle);
+            SetContentView(Resource.Layout.Main);
+            WebView webView = FindViewById<WebView>(Resource.Id.webView);
+            webView.Settings.JavaScriptEnabled = true;
+            webView.SetWebViewClient(new WebViewClient());
+            webView.LoadUrl("https://www.freshaus.co.uk");
+
+        }
+    }
+}
+
